6e4a9d9a21b72f74eb296ad33f32696e8d9aa8ee
FIRDAVS-CMD <77011954+FIRDAVS-CMD@users.noreply.github.com>
2024-05-11 17:03:05 +0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,26 +1,31 @@
-import os
-LANGUAGE_CODE = 'en-us'
+"""
+URL configuration for config project.
 
-TIME_ZONE = 'Asia/Tashkent'
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
 
-USE_I18N = True
+from config import settings
 
-USE_TZ = True
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR / 'static')
-STATICFILES_DIRS = [BASE_DIR]
-
-MEDIA_URL = '/media/'  # new
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # new
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/v1/', include('blog.urls'))
+]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # new
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  # new
