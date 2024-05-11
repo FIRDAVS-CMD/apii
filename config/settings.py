369a8b527058166f215778e67f31ad1936690ff2
FIRDAVS-CMD <77011954+FIRDAVS-CMD@users.noreply.github.com>
2024-05-11 17:26:28 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -153,12 +153,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR / 'static')
-STATICFILES_DIRS = [BASE_DIR]
+STATIC_FILES_DIRS = [BASE_DIR]
 
 MEDIA_URL = '/media/'  # new
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # new
+MEDIA_ROOT = BASE_DIR / 'media'  # new
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
